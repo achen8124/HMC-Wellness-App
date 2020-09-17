059d1f89b8fe5f6490b6a369b38588586fc71ac6
Andrea <andchen@hmc.edu>
2020-09-17 12:39:26 -0700

    I think I messed up a lot, but added a service worker, transfered stuff over from index.js and index.css to App.js and App.css as well.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import React, {Component} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Welcome!</h1>
-        <p id="instructions">There's not enough stuff here, if we add more text, then the gradient 
-          will extend uwuuuuuuuuuu</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-          sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    )
-  }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
