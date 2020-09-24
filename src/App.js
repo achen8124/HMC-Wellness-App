2d43a10d3e24af6f1508f575026f7dd638edf415
Andrea <andchen@hmc.edu>
2020-09-24 12:34:58 -0700

    Update App.js
    Changed App to be a class so that we could nest components later, hopefully it doesn't mess up anything that you had @aldrenaline ! Also added a temp navigation bar (it's not actually a navigation bar, just putting it there as a placeholder LOL

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './App.css'
-export {App, Calendar}
 
-const App = () => {
-    return (
+
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return (
         <div className="main-container">
             <h1>uh oh</h1>
             <button onClick={function() {ReactDOM.render(<Calendar />, document.getElementById('root'))}}> Go to home </button>
+            <NavBar />
         </div>
-    );
+        );
+    }
 }
 
 const Calendar = () => {
@@ -20,3 +26,13 @@ const Calendar = () => {
         </div>
     )
 }
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>let's implement this later</p>
+        </div>
+    )
+}
+
+export {App, Calendar, NavBar}
