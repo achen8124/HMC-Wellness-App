9ae9bcbda598914f1258f5f3168035c4a63353eb
Aldrin Feliciano <aldrenaline369@gmail.com>
2020-09-24 12:52:11 -0600

    add button that goes to calendar page and back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import './App.css'
+export {App, Calendar}
 
 const App = () => {
     return (
         <div className="main-container">
             <h1>uh oh</h1>
+            <button onClick={function() {ReactDOM.render(<Calendar />, document.getElementById('root'))}}> Go to home </button>
         </div>
     );
 }
-export default App;
+
+const Calendar = () => {
+    return (
+        <div>
+            <h1> Calendar? </h1>
+            <button onClick={function() {ReactDOM.render(<App />, document.getElementById('root'))}}> Go to calendar </button>
+        </div>
+    )
+}
