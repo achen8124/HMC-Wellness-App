acd77fd94ec65dd7b8850d75e8d9610d2fbb41bf
Aldrin Feliciano <aldrenaline369@gmail.com>
2020-10-08 11:54:26 -0600

    add navbar that takes user to different pages

--- src/App.js
+++ src/App.js
@@ -10,29 +10,84 @@ class App extends React.Component {
     render() {
         return (
         <div className="main-container">
-            <h1>uh oh</h1>
-            <button onClick={function() {ReactDOM.render(<Calendar />, document.getElementById('root'))}}> Go to home </button>
+            <h1>Home Page</h1>
             <NavBar />
         </div>
         );
     }
 }
 
-const Calendar = () => {
-    return (
-        <div>
-            <h1> Calendar? </h1>
-            <button onClick={function() {ReactDOM.render(<App />, document.getElementById('root'))}}> Go to calendar </button>
-        </div>
-    )
+class Reminders extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Reminders? </h1>
+                <NavBar />
+            </div>
+        );
+    }
 }
+
+class Resources extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Resources </h1>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+class Exercises extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Exercises </h1>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+class Settings extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Settings </h1>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
 
 const NavBar = () => {
     return (
         <div>
             <p>let's implement this later</p>
+            <button onClick={function() {
+                ReactDOM.render(<App />, document.getElementById('root'))
+                }}> Home Page 
+            </button>
+            <button onClick={function() {
+                ReactDOM.render(<Reminders />, document.getElementById('root'))
+                }}> Reminders 
+            </button>
+            <button onClick={function() {
+                ReactDOM.render(<Resources />, document.getElementById('root'))
+                }}> Resources 
+            </button>
+            <button onClick={function() {
+                ReactDOM.render(<Exercises />, document.getElementById('root'))
+                }}> Exercises 
+            </button>
+            <button onClick={function() {
+                ReactDOM.render(<Settings />, document.getElementById('root'))
+                }}> Settings 
+            </button>
         </div>
     )
 }
 
-export {App, Calendar, NavBar}
+export {App, NavBar}
